aece4d6469ce42235395078555ce5cd603d762f7
Andrew <andrew@andrew-dell.(none)>
2011-12-08 14:19:23 -0500

    Adding files

---
+++ t1/requestHandlers.js
@@ -0,0 +1,13 @@
+/* Some "controller" handler functions
+ *
+ **/
+function start() {
+  console.log("Request handler 'start' was called.");
+}
+
+function upload() {
+  console.log("Request handler 'upload' was called.");
+}
+
+exports.start = start;
+exports.upload = upload;
