a87740c3924ff6fb27f9632f76ecc8a2ae10723c
Andrew Drane <andrew@andrewdrane.co>
2011-12-10 14:24:20 -0500

    Chat server simple from git://github.com/ry/node_chat.git

--- express1/routes/users.js
+++ express1/routes/users.js
@@ -7,28 +7,33 @@
 var models = require( '../models/user' )
 
 //index of users
+//@TODO - make this a paginated search
 exports.index = function( req, res ) {
     data = {}; //handy dandy way to keep track of some data
+    
     //find the user by id
     models.User.find(1)
     .on('success', function(user){
         data.myUser = u;
     });
-//        
-        console.log('here');
-        //console.log(myUser);
-//    
-//    
-//    
-//    
-//    
-//    
-    
+      
     
     res.render('index', { 
         view: __dirname + '/views/andrew/andrew',
         dirname: __dirname,
-        title: 'Users function',
-        mcValue: "Wow - it's just like loading a controller!!"
+        title: 'Got User ' + data.myUser.name
     })
 }
+
+//get a specific user
+exports.user = function(){
+    
+    
+}
+
+
+//@TODO
+// functions
+//  -- add
+//  -- delete
+//  -- login (SESSIONS ZOMFG!)
